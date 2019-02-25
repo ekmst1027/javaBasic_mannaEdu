b6ec0b06ac1a81acf928e589d3acc35a6762cf23
KyeongMin <ekmst1027@gmail.com>
2019-02-25 09:38:23 +0900

    java Hello

---
+++ src/ch01/Hello.java
@@ -0,0 +1,13 @@
+package ch01;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("첫번째 자바 프로그램");
+	}
+}
+// public : 모든 곳에서 접근 가능
+// 클래스 이름 : 대소문자 구분
+// class Hello => Hello.java
+// static : 정적인 요소
+// void : 리턴값(되돌려주는 값)이 없음
+// ; 세미콜론 => 문장의 끝에 표시
