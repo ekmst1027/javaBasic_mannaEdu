a95c0f91e1ff6b1d92e47dbed61570c54c8a26be
kyeongmin <ekmst1027@gmail.com>
2019-03-07 10:26:49 +0900

    java Canvas and JProgressBar

---
+++ src/ch17_gui/CanvasExam.java
@@ -0,0 +1,48 @@
+package ch17_gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Canvas;
+
+// BallCanvas 클래스 작성
+// 추가 코드
+public class CanvasExam extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CanvasExam frame = new CanvasExam();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public CanvasExam() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		BallCanvas canvas = new BallCanvas();
+		contentPane.add(canvas, BorderLayout.CENTER);
+	}
+
+}
